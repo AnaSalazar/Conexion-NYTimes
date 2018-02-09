d6859bcd2b3d3300540bb49de89d82ae1f411af0
AnaSalazar <anastephany.salazarguzman@gmail.com>
2018-02-09 10:04:55 -0600

    Actualiza lesson-1 / versión DEMO

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -1,12 +1,37 @@
-(function () {
-    const form = document.querySelector('#search-form');
-    const searchField = document.querySelector('#search-keyword');
-    let searchedForText;
-    const responseContainer = document.querySelector('#response-container');
+const form = document.getElementById('search-form');
+const searchField = document.getElementById('search-keyword');
+const responseContainer = document.getElementById('response-container');
+let searchedForText;
 
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        responseContainer.innerHTML = '';
-        searchedForText = searchField.value;
-    });
-})();
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    responseContainer.innerHTML = '';
+    searchedForText = searchField.value;
+    getNews();
+});
+
+function getNews() {
+  const articleRequest = new XMLHttpRequest();
+  articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=a4506c2f7f2243d599a5b3857fd768ff`);
+  articleRequest.onload = addNews;
+  articleRequest.onerror = handleError;
+  articleRequest.send();
+}
+
+function addNews(){
+  const data = JSON.parse(this.responseText);
+  const article = data.response.docs[0];
+  const title = article.headline.main;
+  const snippet = article.snippet;
+
+  let li = document.createElement('li');
+  li.className = 'articleClass';
+  li.innerText = snippet;
+
+  responseContainer.appendChild(li);
+}
+
+
+function handleError() {
+  console.log('Se ha presentado un error');
+}
